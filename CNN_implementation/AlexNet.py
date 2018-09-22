82c0d893993e42e4cc1b044bb2381626b96c9436
仲辞 <zefeng.zzf@antfin.com>
2018-09-22 18:43:28 +0800

    alex net implementation

---
+++ CNN_implementation/AlexNet.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+
+'''
+  AlexNet 结构
+  输入图片尺寸为224 * 224
+  5层卷积 + 3层全连接
+  卷积1, 11*11, 步长4, output channel = 96
+  LRN
+  最大池化，3*3， 步长2
+'''
+
+from datetime import datetime
+import math
+import time
+import tensorflow as tf
+
+# hyper parameters
+batch_size = 32
+num_batches = 100
+
+# helper function, show input tensor's name and shape
+def print_activations(t):
+  print(t.op.name, ' -> ', t.get_shape().as_list())
+
+
+def inference(images):
+  parameters = []
+
+  # 卷积层一
+  with tf.name_scope('conv1') as scope:
+    # 设置卷积核
+    kernel = tf.Variable(tf.truncated_normal([11, 11, 3, 64], dtype = tf.float32, stddev = 1e-1), name = 'weights')
+
+    # 执行卷积操作
+    conv = tf.nn.conv2d(images, kernel, [1,4,4,1], padding = 'SAME')
+
+    # 设置偏差值
+    biases = tf.Variable(tf.constant(0.0, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+
+    # 执行偏差值添加操作
+    bias = tf.nn.bias_add(conv, biases)
+
+    # ReLU激活函数
+    conv1 = tf.nn.relu(bias, name=scope)
+
+    # 打印结构
+    print_activations(conv1)
+
+    # 添加可训练的参数到parameters中
+    parameters += [kernel, biases]
+
+  # LRN层
+  lrn1 = tf.nn.lrn(conv1, 4, bias=1.0, alpha=0.001/9, beta=0.75, name='lrn1')
+
+  # 最大池化层, VALID表示取样时不能超过边框
+  pool1 = tf.nn.max_pool(lrn1, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID', name='pool1')
+
+  print_activations(pool1)
+
+  # 卷积层二, 卷积核变为5*5， 输入64， 输出192
+  with tf.name_scope('conv2') as scope:
+    kernel = tf.Variable(tf.truncated_normal([5,5,64,192], dtype=tf.float32, stddev=1e-1), name='weights')
+    conv = tf.nn.conv2d(pool1, kernel, [1,1,1,1], padding='SAME')
+    biases = tf.Variable(tf.constant(0.0, shape=[192], dtype=tf.float32), trainable=True, name='biases')
+    bias = tf.nn.bias_add(conv, biases)
+    conv2 = tf.nn.relu(bias, name=scope)
+    parameters += [kernel, biases]
+    print_activations(conv2)
+  lrn2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001/9, beta=0.75, name='lrn2')
+  pool2 = tf.nn.max_pool(lrn2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID', name='pool2')
+  print_activations(pool2)
+
+  # 卷积层三, 卷积核变为3*3， 输入192， 输出384
+  with tf.name_scope('conv3') as scope:
+    kernel = tf.Variable(tf.truncated_normal([3,3,192,384], dtype=tf.float32, stddev=1e-1), name='weights')
+    conv = tf.nn.conv2d(pool2, kernel, [1,1,1,1], padding='SAME')
+    biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32), trainable=True, name='biases')
+    bias = tf.nn.bias_add(conv, biases)
+    conv3 = tf.nn.relu(bias, name=scope)
+    parameters += [kernel, biases]
+    print_activations(conv3)
+
+  # 卷积层四, 输入384， 输出256
+  with tf.name_scope('conv4') as scope:
+    kernel = tf.Variable(tf.truncated_normal([3,3,384,256], dtype=tf.float32, stddev=1e-1), name='weights')
+    conv = tf.nn.conv2d(conv3, kernel, [1,1,1,1], padding='SAME')
+    biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+    bias = tf.nn.bias_add(conv, biases)
+    conv4 = tf.nn.relu(bias, name=scope)
+    parameters += [kernel, biases]
+    print_activations(conv4)
+
+  # 卷积层五
+  with tf.name_scope('conv5') as scope:
+    kernel = tf.Variable(tf.truncated_normal([3,3,256,256], dtype=tf.float32, stddev=1e-1), name='weights')
+    conv = tf.nn.conv2d(conv4, kernel, [1,1,1,1], padding='SAME')
+    biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), trainable=True, name='biases')
+    bias = tf.nn.bias_add(conv, biases)
+    conv5 = tf.nn.relu(bias, name=scope)
+    parameters += [kernel, biases]
+    print_activations(conv5)
+  pool5 = tf.nn.max_pool(conv5, ksize=[1,3,3,1], strides=[1,2,2,1], padding='VALID', name='pool5')
+  print_activations(pool5)
+
+  # 全连接reshape
+  reshape = tf.reshape(pool5, [batch_size, -1])
+  dim = reshape.get_shape()[1].value
+
+  # 全连接一
+  with tf.name_scope('fc1') as scope:
+    weights = tf.Variable(tf.truncated_normal([dim, 4096], dtype=tf.float32, stddev=0.04), name='weights')
+    bias = tf.Variable(tf.constant(0.0, shape=[4096], dtype=tf.float32, trainable=True, name='bias'))
+    fc1 = tf.nn.relu(tf.matmul(reshape, weights) + bias)
+    parameters += [weights, bias]
+  
+  # 全连接二
+  with tf.name_scope('fc2') as scope:
+    weights = tf.Variable(tf.truncated_normal([4096, 4096], dtype=tf.float32, stddev=0.04), name='weights')
+    bias = tf.Variable(tf.constant(0.0, shape=[4096], dtype=tf.float32, trainable=True, name='bias'))
+    fc2 = tf.nn.relu(tf.matmul(fc1, weights) + bias)
+    parameters += [weights, bias]
+
+
+  # 全连接三
+  with tf.name_scope('fc3') as scope:
+    weights = tf.Variable(tf.truncated_normal([4096, 1024], dtype=tf.float32, stddev=0.04), name='weights')
+    bias = tf.Variable(tf.constant(0.0, shape=[1024], dtype=tf.float32, trainable=True, name='bias'))
+    fc3 = tf.nn.relu(tf.matmul(fc2, weights) + bias)
+    parameters += [weights, bias]
+
+  return fc3, parameters
+
--- hwj_book/complexCNN.py
+++ hwj_book/complexCNN.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+
 import cifar10, cifar10_input
 import tensorflow as tf 
 import numpy as np
@@ -7,3 +10,119 @@ import time
 max_steps = 3000
 hatch_size = 128
 data_dir = '/tmp/cifar10_data/cifar-10-batches-bin'
+
+def variable_with_weight_loss(shape, stddev, wl):
+  var = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+  if wl is not None:
+    weight_loss = tf.multiply(tf.nn.l2_loss(var), wl, name='weight_loss')
+    tf.add_to_collection('losses', weight_loss)
+  return var
+
+cifar10.maybe_download_and_extract()
+
+# generate training set
+images_train, labels_train = cifar10_input.distorted_inputs(data_dir = data_dir, batch_size = batch_size)
+
+# generate test set
+images_test, labels_test = cifar10_input.inputs(eval_data = True, data_dir = data_dir, batch_size = batch_size)
+
+# placeholder for inputs
+
+image_holder = tf.placeholder(tf.float32, [batch_size, 24, 24, 3])
+label_holder = tf.placeholder(tf.float32, [batch_size])
+
+# ------ Phase 1: construct network model, give inference -----------
+# layer 1 - convolution
+
+weight_1 = variable_with_weight_loss(shape=[5,5,3,64], stddev=5e-2, wl=0.0)
+kernel_1 = tf.nn.conv2d(image_holder, weight_1, [1,1,1,1], padding='SAME')
+bias_1 = tf.Variable(tf.constant(0.0, shape=[64]))
+conv_1 = tf.nn.relu(tf.nn.bias_add(kernel_1, bias_1))
+pool_1 = tf.nn.max_pool(conv_1, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
+norm_1 = tf.nn.lrn(pool_1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
+
+# layer 2 - convolution
+
+weight_2 = variable_with_weight_loss(shape=[5,5,64,64], stddev=5e-2, wl=0.0)
+kernel_2 = tf.nn.conv2d(norm_1, weight_2, [1,1,1,1], padding='SAME')
+bias_2 = tf.Variable(tf.constant(0.1, shape=[64]))
+conv_2 = tf.nn.relu(tf.nn.bias_add(kernel_2, bias_2))
+norm_2 = tf.nn.lrn(conv_2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
+pool_2 = tf.nn.max_pool(norm_2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
+
+# layer 3 - full connect
+
+reshape = tf.reshape(pool_2, [batch_size, -1])
+dim = reshape.get_shape()[1].value
+print '====='
+print dim
+weight_3 = variable_with_weight_loss(shape=[dim, 384], stddev=0.04, wl=0.004)
+bias_3 = tf.Variable(tf.constant(0.1, shape=[384]))
+local_3 = tf.nn.relu(tf.matmul(reshape, weight_3) + bias_3)
+
+# layer 4 - full connect
+
+weight_4 = variable_with_weight_loss(shape=[384, 192], stddev=0.04, wl=0.004)
+bias_4 = tf.Variable(tf.constant(0.1, shape=[192]))
+local_4 = tf.nn.relu(tf.matmul(local_3, weight_4) + bias_4)
+
+# layer 5 - give inference
+
+weight_5 = variable_with_weight_loss(shape[192, 10], stddev=1/192.0, wl=0.0)
+bias_5 = tf.Variable(tf.constant(0.0, shape=[10]))
+logits = tf.add(tf.matmul(local_4, weight_5), bias_5)
+
+
+# ------ Phase 2: define loss -----------
+def loss(logits, labels):
+  labels = tf.cast(labels, tf.int64)
+  cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+    logits=logits, labels=labels, name='cross_entropy_per_example'
+  )
+  cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+
+  tf.add_to_collection('losses', cross_entropy_mean)
+  return tf.add_n(tf.get_collection('losses'), name='total_loss')
+
+loss = loss(logits, label_holder)
+
+# ------ Phase 3: define optimizer -----------
+optimizer = tf.train.AdamOptimizer(1e-3).minimize(loss)
+
+
+# ------ Phase 4: train -----------
+top_k_op = tf.nn.in_top_k(logits, label_holder, 1)
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+tf.train.start_queue_runners()
+
+for step in range(max_steps):
+  start_time = time.time()
+  image_batch, label_batch = sess.run([images_train, labels_train])
+  _, loss_value = sess.run([optimizer, loss], feed_dict={image_holder: image_batch, label_holder: label_batch})
+  duration = time.time() - start_time
+
+  if step % 10 == 0:
+    examples_per_sec = batch_size / duration
+    sec_per_batch = float(duration)
+
+    format_str = ('step %d, loss = %.4f (%.1f examples/sec; %.3f sec/batch)')
+    print (format_str % (step, loss_value, examples_per_sec, sec_per_batch))
+
+
+# ------ Phase 5: run on test data -----------
+num_examples = 10000
+import math
+num_iter = int(math.ceil(num_examples / batch_size))
+true_count = 0
+total_sample_count = num_iter * batch_size
+step = 0
+while step < num_iter:
+  image_batch, label_batch = sess.run([images_test, labels_test])
+  predictions = sess.run([top_k_op], feed_dict={image_holder: image_batch, label_holder: label_batch})
+  true_count += np.sum(predictions)
+  step += 1
+
+precision = true_count / total_sample_count
+print ('precision @ 1 = %.4f' % precision)
